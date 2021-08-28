281d1c22d3e67d7ae65ed657dab80d21202a0a58
pyskonus <pyskob190201@gmail.com>
2021-08-28 16:42:02 +0300

    arsenal

--- ShootThemUp/Private/Components/STUWeaponComponent.cpp
+++ ShootThemUp/Private/Components/STUWeaponComponent.cpp
@@ -15,24 +15,56 @@ void USTUWeaponComponent::BeginPlay()
 {
 	Super::BeginPlay();
   
-    SpawnWeapon();
+    SpawnWeapons();
+
+    EquipWeapon(CurrentWeaponIndex);
 }
 
-void USTUWeaponComponent::SpawnWeapon() {
-  if (!GetWorld())
+void USTUWeaponComponent::EndPlay(const EEndPlayReason::Type EndPlayReason){
+  CurrentWeapon = nullptr;
+  for (auto Weapon : Weapons) {
+    Weapon->DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
+    Weapon->Destroy();
+  }
+  Weapons.Empty();
+  Super::EndPlay(EndPlayReason);
+};
+
+void USTUWeaponComponent::SpawnWeapons() {
+  ACharacter* Character = Cast<ACharacter>(GetOwner());
+  if (!Character || !GetWorld())
     return;
 
+  for (auto WeaponClass : WeaponClasses) {
+    auto Weapon = GetWorld()->SpawnActor<ASTUBaseWeapon>(WeaponClass);
+    if (!Weapon)
+      continue;
+
+    Weapon->SetOwner(Character);
+    Weapons.Add(Weapon);
+    AttachWeaponToSocket(Weapon, Character->GetMesh(), WeaponArmorySocketName);
+  }
+}
+
+void USTUWeaponComponent::AttachWeaponToSocket(ASTUBaseWeapon* Weapon, USceneComponent* SceneComponent, const FName& SocketName) {
+  if (!Weapon || !SceneComponent)
+    return;
+  FAttachmentTransformRules AttachmentRules(EAttachmentRule::SnapToTarget, false);
+  Weapon->AttachToComponent(SceneComponent, AttachmentRules, SocketName);
+}
+
+void USTUWeaponComponent::EquipWeapon(int32 WeaponIndex) {
   ACharacter* Character = Cast<ACharacter>(GetOwner());
   if (!Character)
     return;
 
-  CurrentWeapon = GetWorld()->SpawnActor<ASTUBaseWeapon>(WeaponClass);
-  if (!CurrentWeapon)
-    return;
+  if (CurrentWeapon) {
+    CurrentWeapon->StopFire();
+    AttachWeaponToSocket(CurrentWeapon, Character->GetMesh(), WeaponArmorySocketName);
+  }
 
-  FAttachmentTransformRules AttachmentRules(EAttachmentRule::SnapToTarget, false);
-  CurrentWeapon->AttachToComponent(Character->GetMesh(), AttachmentRules, WeaponAttachmentPointName);
-  CurrentWeapon->SetOwner(Character);
+  CurrentWeapon = Weapons[WeaponIndex];
+  AttachWeaponToSocket(CurrentWeapon, Character->GetMesh(), WeaponEquipSocketName);
 }
 
 void USTUWeaponComponent::StartFire() {
@@ -47,4 +79,10 @@ void USTUWeaponComponent::StopFire() {
     return;
 
   CurrentWeapon->StopFire();
+}
+
+void USTUWeaponComponent::NextWeapon() {
+  CurrentWeaponIndex = (CurrentWeaponIndex + 1) % Weapons.Num();
+
+  EquipWeapon(CurrentWeaponIndex);
 }
--- ShootThemUp/Private/STUBaseCharacter.cpp
+++ ShootThemUp/Private/STUBaseCharacter.cpp
@@ -71,6 +71,7 @@ void ASTUBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
   PlayerInputComponent->BindAction("Run", IE_Released, this, &ASTUBaseCharacter::OnStopRunning);
   PlayerInputComponent->BindAction("Fire", IE_Pressed, WeaponComponent, &USTUWeaponComponent::StartFire);
   PlayerInputComponent->BindAction("Fire", IE_Released, WeaponComponent, &USTUWeaponComponent::StopFire);
+  PlayerInputComponent->BindAction("NextWeapon", IE_Pressed, WeaponComponent, &USTUWeaponComponent::NextWeapon);
 }
 
 bool ASTUBaseCharacter::IsRunning() const {
@@ -117,6 +118,7 @@ void ASTUBaseCharacter::OnDeath() {
   }
 
   GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECR_Ignore);
+  WeaponComponent->StopFire();
 }
 
 void ASTUBaseCharacter::OnHealthChange(float Health) {
