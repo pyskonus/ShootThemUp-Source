834e354903d170121a221052d95d4e56b456e9ae
pyskonus <pyskob190201@gmail.com>
2021-09-21 21:45:05 +0300

    AI

---
+++ ShootThemUp/Public/AI/STUAICharacter.h
@@ -0,0 +1,18 @@
+// Created by pyskonus.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "STUBaseCharacter.h"
+#include "STUAICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API ASTUAICharacter : public ASTUBaseCharacter {
+  GENERATED_BODY()
+
+public:
+  ASTUAICharacter(const FObjectInitializer& ObjInit);
+};
---
+++ ShootThemUp/Public/AI/STUAIController.h
@@ -0,0 +1,17 @@
+// Created by pyskonus.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "STUAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API ASTUAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
