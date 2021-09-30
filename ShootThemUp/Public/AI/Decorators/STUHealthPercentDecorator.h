0d0c21d2ba258e6ff011c6a9c839566184d2407a
pyskonus <pyskob190201@gmail.com>
2021-09-30 17:30:56 +0300

    health pickup

---
+++ ShootThemUp/Public/AI/Decorators/STUHealthPercentDecorator.h
@@ -0,0 +1,21 @@
+// Created by pyskonus.
+
+#pragma once
+
+#include "BehaviorTree/BTDecorator.h"
+#include "CoreMinimal.h"
+#include "STUHealthPercentDecorator.generated.h"
+
+UCLASS()
+class SHOOTTHEMUP_API USTUHealthPercentDecorator : public UBTDecorator {
+  GENERATED_BODY()
+
+public:
+  USTUHealthPercentDecorator();
+
+protected:
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+  float HealthPercent = 0.6f;
+
+  virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
