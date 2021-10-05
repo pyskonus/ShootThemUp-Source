6add2a2c9526cfa1105c159c96ee7bd25eb174d2
pyskonus <pyskob190201@gmail.com>
2021-10-05 21:39:18 +0300

    time, rounds and kills widget

--- ShootThemUp/Public/UI/STUGameDataWidget.h
+++ ShootThemUp/Public/UI/STUGameDataWidget.h
@@ -2,16 +2,31 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "CoreMinimal.h"
 #include "STUGameDataWidget.generated.h"
 
-/**
- * 
- */
+class ASTUGameModeBase;
+class ASTUPlayerState;
+
 UCLASS()
-class SHOOTTHEMUP_API USTUGaneDataWidget : public UUserWidget
-{
-	GENERATED_BODY()
-	
+class SHOOTTHEMUP_API USTUGameDataWidget : public UUserWidget {
+  GENERATED_BODY()
+
+public:
+  UFUNCTION(BlueprintCallable, Category = "UI")
+  int32 GetKillsNum() const;
+
+  UFUNCTION(BlueprintCallable, Category = "UI")
+  int32 GetCurrentRoundNum() const;
+
+  UFUNCTION(BlueprintCallable, Category = "UI")
+  int32 GetTotalRoundsNum() const;
+
+  UFUNCTION(BlueprintCallable, Category = "UI")
+  int32 GetRoundSecondsRemaining() const;
+
+private:
+  ASTUGameModeBase* GetSTUGameMode() const;
+  ASTUPlayerState* GetSTUPlayerState() const;
 };
