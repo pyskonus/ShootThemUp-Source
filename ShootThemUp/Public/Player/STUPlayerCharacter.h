3465a0961a569378d45a47a14654b2564c8f3a27
pyskonus <pyskob190201@gmail.com>
2021-10-06 17:21:32 +0300

    camera-character overlap fix

--- ShootThemUp/Public/Player/STUPlayerCharacter.h
+++ ShootThemUp/Public/Player/STUPlayerCharacter.h
@@ -8,6 +8,7 @@
 
 class UCameraComponent;
 class USpringArmComponent;
+class USphereComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUPlayerCharacter : public ASTUBaseCharacter {
@@ -27,7 +28,11 @@ protected:
   UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
   UCameraComponent* CameraComponent;
 
+  UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+  USphereComponent* CameraCollisionComponent;
+
   virtual void OnDeath() override;
+  virtual void BeginPlay() override;
 
 private:
   bool WantsToRun = false;
@@ -38,4 +43,12 @@ private:
 
   void OnStartRunning();
   void OnStopRunning();
+
+  UFUNCTION()
+  void OnCameraCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+  UFUNCTION()
+  void OnCameraCollisionEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+  void CheckCameraOverlap();
 };
